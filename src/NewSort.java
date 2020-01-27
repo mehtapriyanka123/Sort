86cdb14e568fd54c7c2aad70d90cb9544ef3b924
Piku <Piku@DESKTOP-KL06S5O>
2020-01-27 14:47:51 +0530

    done1

--- src/NewSort.java
+++ src/NewSort.java
@@ -8,7 +8,7 @@ public class NewSort {
             int j = i - 1; 
             while (j >= 0 && arr[j] > key) { 
                 arr[j + 1] = arr[j]; 
-                j = j - 1; 
+                j = j - 1;
             } 
             arr[j + 1] = key; 
         } 
